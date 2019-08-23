8331e1c92525e39676de9a64c296c79f1fba777c
Tyler Schaffer <tpschaffer@gmail.com>
2019-08-23 10:08:16 -0600

    User story 3 passing- needs to be set up to sort alphabetically

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Mission.destroy_all
 Astronaut.destroy_all
-
 
 Astronaut.create!(name: "Buzz Lightyear", age: 24, job: "Space Ranger")
 Astronaut.create!(name: "Laika", age: 5, job: "Space Dog")
+
+Mission.create!(title: "Academy Training", time_in_space: 1300)
+Mission.create!(title: "Attack on Emperor Zurg", time_in_space: 60)
+Mission.create!(title: "Sputnik 2", time_in_space: 250)
--- spec/features/astronaut/index_spec.rb
+++ spec/features/astronaut/index_spec.rb
@@ -1,15 +1,18 @@
 require 'rails_helper'
 
-
-# When I visit '/astronauts'
-# I see a list of astronauts with the following info:
-# - Name
-# - Age
-# - Job
 describe "Astronaut Index Page" do
   before(:each) do
     @buzz = Astronaut.create!(name: "Buzz Lightyear", age: 24, job: "Space Ranger")
     @laika = Astronaut.create!(name: "Laika", age: 5, job: "Space Dog")
+
+    @academy_training = Mission.create!(title: "Academy Training", time_in_space: 1300)
+    @zurg = Mission.create!(title: "Attack on Emperor Zurg", time_in_space: 600)
+    @sputnik = Mission.create!(title: "Sputnik 2", time_in_space: 250)
+
+    @academy_training.astronauts << @buzz
+    @academy_training.astronauts << @laika
+    @zurg.astronauts << @buzz
+    @sputnik.astronauts << @laika
   end
 
   it "lists all astronauts with their name, age, and job" do
@@ -32,5 +35,27 @@ describe "Astronaut Index Page" do
     visit "/astronauts"
 
     expect(page).to have_content("Average Astronaut Age: 14.5")
+  end
+
+  # As a visitor,
+  # When I visit '/astronauts'
+  # I see a list of the space missions' in alphabetical order for each astronaut.
+  #
+  # (e.g "Apollo 13"
+  #      "Capricorn 4"
+  #      "Gemini 7")
+  it "lists all the missions for each astronaut in alphabetical order" do
+    visit "/astronauts"
+
+    within "#astronaut-#{@buzz.id}" do
+      expect(page).to have_content(@academy_training.title)
+      expect(page).to have_content(@zurg.title)
+    end
+
+    within "#astronaut-#{@laika.id}" do
+      expect(page).to have_content(@academy_training.title)
+      expect(page).to have_content(@sputnik.title)
+    end
+
   end
 end
